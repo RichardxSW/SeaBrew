05b694544ac6f7322fe0c9e7506b6654fc0e2e8e
willsenyogi <willsenyp21@gmail.com>
2024-06-06 01:21:31 +0700

    bundle layout progress (3)

--- screens/history.js
+++ screens/history.js
@@ -34,6 +34,42 @@ const History = () => {
                         <Text style={styles.paidText}>Paid</Text>
                     </View>
                 </View>
+                <View style={styles.bundleRow}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.itemName}>Choco Cappuccino Set</Text>
+                        <Text style={styles.visitDate}>
+                            Visit date: 29 May 2024
+                        </Text>
+                    </View>
+                    <View style={styles.groupContainer}>
+                        <Text style={styles.itemPrice}>Rp.190.000</Text>
+                        <Text style={styles.paidText}>Paid</Text>
+                    </View>
+                </View>
+                <View style={styles.bundleRow}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.itemName}>Cappuccino & Americano Combo</Text>
+                        <Text style={styles.visitDate}>
+                            Visit date: 3 June 2024
+                        </Text>
+                    </View>
+                    <View style={styles.groupContainer}>
+                        <Text style={styles.itemPrice}>Rp.150.000</Text>
+                        <Text style={styles.paidText}>Paid</Text>
+                    </View>
+                </View>
+                <View style={styles.bundleRow}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.itemName}>Green Macchiato Delight</Text>
+                        <Text style={styles.visitDate}>
+                            Visit date: 2 June 2024
+                        </Text>
+                    </View>
+                    <View style={styles.groupContainer}>
+                        <Text style={styles.itemPrice}>Rp.210.000</Text>
+                        <Text style={styles.paidText}>Paid</Text>
+                    </View>
+                </View>
             </View>
         </View>
     );
