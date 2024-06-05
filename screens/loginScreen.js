23c54556cc366b4b3cc3218802c6888ae8b84496
TanjayaJason <tanjaya.jason01@gmail.com>
2024-06-05 15:46:49 +0700

    update path background

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -40,7 +40,7 @@ export default function LoginScreen({ navigation }) {
           </View>
 
           <TouchableOpacity style={styles.buttonLoginContainer}
-            // onPress={() => navigation.navigate('Main')}
+            onPress={() => navigation.navigate('Home')}
           >
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
