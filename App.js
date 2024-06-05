746976b8cc57eefa6c8e23cd1ce39ad1cc63d688
TanjayaJason <tanjaya.jason01@gmail.com>
2024-06-05 12:52:05 +0700

    button login and google

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screen/loginScreen';
+import LoginScreen from './screens/loginScreen';
 
 const Stack = createStackNavigator();
 
--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function LoginScreen({ navigation }) {
   return (
     <ImageBackground source={require('../assets/Background.png')} style={styles.background}>
       <View style={styles.container}>
-        <Text style={styles.text}>Login</Text>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Login"
-            // onPress={() => navigation.navigate('Main')}
-            color="#375A82"
+        <Text style={styles.titletext}>Login</Text>
+
+        <TouchableOpacity style={styles.buttonLoginContainer}
+          // onPress={() => navigation.navigate('Main')}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.text}>
+          Or Sign In With
+        </Text>
+
+        <TouchableOpacity style={styles.buttonGoogleContainer}>
+          <Image
+            source={require('../assets/googleicon.png')} // Ubah path gambar sesuai dengan lokasi dan nama file gambar Anda
+            style={styles.iconStyle} // Style untuk mengatur ukuran dan posisi ikon
           />
-        </View>
+          <Text style={styles.buttonText}>Google</Text>
+        </TouchableOpacity>
+
         <StatusBar style="auto" />
       </View>
     </ImageBackground>
@@ -30,20 +42,56 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
+  titletext: {
     position: 'absolute',
-    top: 110,  // Adjust this value to position the text vertically
+    top: 110,
     textAlign: 'center',
     fontSize: 40,
     fontWeight: '100',
     color: '#375A82',
     marginBottom: 20,
   },
-  buttonContainer: {
-    width: '80%',
+  buttonLoginContainer: {
+    width: '70%',
     borderRadius: 10,
     overflow: 'hidden',
-    marginTop: 20, // tambahkan margin atas agar tombol tidak terlalu dekat dengan teks
-    paddingVertical: 10, // tambahkan padding vertikal agar tombol lebih terlihat
+    marginTop: 50,
+    marginBottom: 20,
+    paddingVertical: 10,
+    backgroundColor: '#70B5F9',
   },
+  buttonText: {
+    textAlign: 'center',
+    color: '#375A82',
+    fontSize: 16,
+  },
+  buttonGoogleContainer: {
+    flexDirection: 'row', // Mengatur layout menjadi horizontal
+    alignItems: 'center', // Mengatur agar ikon dan teks berada di tengah secara vertikal
+    justifyContent: 'center',
+    width: '40%',
+    overflow: 'hidden',
+    marginTop: 20,
+    paddingVertical: 10,
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  iconStyle: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#375A82',
+    fontSize: 14,
+  }
 });
