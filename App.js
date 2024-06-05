c52075b80f249eaf84b9e19e25915f8d763f9ec6
Jason Sutanto <jasonsutanto665@gmail.com>
2024-06-05 13:56:37 +0700

    Show list page done
    Adding fonts, show popup, rename  folder, moving show info to data folder

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import SplashScreen from './screen/splashScreen';
+import { StyleSheet} from 'react-native';
+import { useFonts } from 'expo-font';
+import SplashScreen from './screens/splashScreen';
+import ShowList from './screens/showList';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded, fontError] = useFonts({
+    'MontserratBold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'MontserratSemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    'MontserratMedium': require('./assets/fonts/Montserrat-Medium.ttf'),
+  });
 
   useEffect(() => {
     async function prepare() {
       try {
         // Pre-load any resources or data that you need prior to rendering the app
-        await new Promise(resolve => setTimeout(resolve, 1500)); // Simulating a delay
+        await new Promise(resolve => setTimeout(resolve, 2150)); // Simulating a delay
       } catch (e) {
         console.warn(e);
       } finally {
@@ -25,11 +36,30 @@ export default function App() {
     return <SplashScreen />;
   }
 
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'black' }}>Welcome to SeaBrew!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="ShowList" 
+          component={ShowList} 
+          options={{
+            title: 'All Show',
+            headerStyle: {
+              backgroundColor: '#92DAFD',
+            },
+            headerTintColor: 'black',
+            headerTitleStyle: {
+              fontFamily: 'MontserratBold',
+              fontSize: 24,
+            },
+            headerTitleAlign: 'center',
+          }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ assets/data/showdata.js
@@ -0,0 +1,60 @@
+const shows = [
+    { 
+      id: 1, 
+      name: 'Meerkat Feeding', 
+      image: require('../shows/meerkat.png'), 
+      description: 'Join us for an immersive experience at the "Meerkat Feeding" show. Watch as our adorable meerkats scavenge for food in their natural habitat, showcasing their teamwork and agility. This interactive presentation offers fascinating insights into the lives of these curious creatures. Dont miss the chance to witness the lively antics of our meerkat family!', 
+      location: 'Desert Exhibit' 
+    },
+    { 
+      id: 2, 
+      name: "Tropical Bird's Encounter", 
+      image: require('../shows/bird.png'), 
+      description: 'Step into the enchanting world of avian wonders at the Tropical Birds Encounter show. Get up close and personal with some of our most vibrant feathered friends as they showcase their dazzling colors and melodious songs. Learn about their unique behaviors, habitats, and the conservation efforts needed to protect these beautiful birds. Join us for an unforgettable journey through the tropical skies!', 
+      location: 'Aviary' 
+    },
+    { 
+      id: 3, 
+      name: 'Mystic Mermaid: Jewel of the Sea', 
+      image: require('../shows/mermaid1.png'), 
+      description: 'Embark on a magical underwater adventure with the mesmerizing mermaids of the Mystic Mermaid: Jewel of the Sea show. Dive into the mystical depths of our mermaid tank and be transported to a world of wonder and enchantment. Experience their graceful movements, breathtaking performances, and discover the secrets of the ocean as told by these legendary creatures. Join us for an aquatic spectacle like no other!', 
+      location: 'Mermaid Tank' 
+    },
+    { 
+      id: 4, 
+      name: 'Otter Adventure Feeding', 
+      image: require('../shows/otter.png'), 
+      description: 'Join the fun-loving otters at the Otter Adventure Feeding show for an unforgettable dining experience. Watch as these playful creatures frolic and feast on a delicious meal, showcasing their agility and intelligence. Learn about otter conservation efforts and the importance of protecting their natural habitats. Dive into the world of otters and make memories that will last a lifetime!', 
+      location: 'Otter Exhibit' 
+    },
+    { 
+      id: 5, 
+      name: 'Penguin Parade', 
+      image: require('../shows/penguin.png'), 
+      description: 'Experience the charm of the Antarctic at the Penguin Parade show. Watch in awe as our adorable penguins waddle and swim in their icy enclosure, entertaining visitors with their playful antics. Learn about the unique adaptations that allow these fascinating birds to thrive in their cold habitat and the conservation efforts in place to protect their future. Join us for a heartwarming journey to the South Pole!', 
+      location: 'Penguin Exhibit' 
+    },
+    { 
+      id: 6, 
+      name: 'Piranha Frenzy Feeding', 
+      image: require('../shows/piranha.png'), 
+      description: 'Get ready for an adrenaline-pumping adventure at the Piranha Frenzy Feeding show. Witness the thrilling spectacle as piranhas feed with incredible speed and precision, dispelling myths and revealing the truth about these infamous fish. Explore the behavior and ecology of these fascinating predators as our expert guides provide insights into their natural habitat and survival strategies. Join us for an unforgettable journey into the depths of the Amazon!', 
+      location: 'Amazon Exhibit' 
+    },
+    { 
+      id: 7, 
+      name: "Predator's Meal", 
+      image: require('../shows/shark.png'), 
+      description: 'Dive into the depths of the ocean and witness the power and grace of our sharks at the Predator\'s Meal show. Experience the thrill as these majestic creatures glide through the water during their feeding time, showcasing their incredible hunting skills. Learn about shark conservation efforts and the importance of protecting these apex predators for the health of our oceans. Join us for an exhilarating adventure beneath the waves!', 
+      location: 'Shark Tank' 
+    },
+    { 
+      id: 8, 
+      name: 'Mermaid Legends: Guardian of the Reef', 
+      image: require('../shows/mermaid2.png'), 
+      description: 'Immerse yourself in the enchanting world of mermaids at the Mermaid Legends: Guardian of the Reef show. Experience a captivating performance that celebrates the beauty and mystery of the ocean, as told through the mesmerizing tales of these mythical beings. Journey to the depths of the sea and discover the wonders that lie beneath the waves. Join us for a magical adventure that will leave you spellbound!', 
+      location: 'Mermaid Lagoon' 
+    },
+  ];
+
+  export default shows;
