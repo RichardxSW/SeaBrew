bc9fe27409298ecb300e720ff561729dba907c23
willsenyogi <willsenyp21@gmail.com>
2024-06-07 15:12:22 +0700

    added top navigation bar to locate active tickets screen and history screen

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import History from './screens/history';
+import React from "react";
+import { StyleSheet, StatusBar, View, Platform } from "react-native"; // Import StatusBar and Platform
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ScreenTabs from "./screens/screenTabs";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <History />
-    </View>
+    <SafeAreaProvider>
+      <GestureHandlerRootView>
+        <View style={styles.container}>
+          <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+          <NavigationContainer>
+            <ScreenTabs />
+          </NavigationContainer>
+        </View>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "white",
   },
 });
